8bb16ce12c18ab7ac644bf557f56b65b017ec8b2
Vincent Bach <vincent.bach@chauffeur-prive.com>
2017-04-11 13:16:53 +0200

    Setup a basic view controller

--- XamarinUITestPoc/ViewController.swift
+++ XamarinUITestPoc/ViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+final class ViewController: UIViewController {
+
+    private let titleLabel: UILabel
+
+    // MARK: - Initializers
+
+    init() {
+        self.titleLabel = UILabel()
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    // MARK: - View life cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        setupView()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    // MARK: - Setup
+
+    private func setupView() {
+        view.addSubview(titleLabel)
+
+        NSLayoutConstraint.activate([
+            titleLabel.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 10.0),
+            titleLabel.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -10.0),
+            titleLabel.centerYAnchor.constraint(equalTo: view.centerYAnchor)
+        ])
+
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false
+        titleLabel.textAlignment = .center
+        titleLabel.text = "Title"
     }
-
-
 }
 
